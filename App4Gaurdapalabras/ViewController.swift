064878602a950d7666aa2329d4c171e4279bb085
EP <droid@MacBook-Pro-de-EP.local>
2017-08-18 17:28:12 -0700

    Se concluyo persistencia de datos

---
+++ App4Gaurdapalabras/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  App4Gaurdapalabras
+//
+//  Created by EP on 17/08/17.
+//  Copyright © 2017 EP. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    
+    
+   
+}
+
